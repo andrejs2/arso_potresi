95c5fe49f82ad45aae53cea966bbcd417f408811
Andrej <44124120+andrejs2@users.noreply.github.com>
2025-08-07 14:51:28 +0200

    Update config_flow.py

--- custom_components/arso_potresi/config_flow.py
+++ custom_components/arso_potresi/config_flow.py
@@ -1,8 +1,9 @@
 import voluptuous as vol
 from homeassistant import config_entries
 from homeassistant.core import callback
+from homeassistant.config_entries import OptionsFlowWithConfigEntry
 
-from .const import DOMAIN, DEFAULT_SCAN_INTERVAL
+from .const import DOMAIN, DEFAULT_SCAN_INTERVAL, DEFAULT_HISTORY_DAYS
 
 class ArsoPotresiConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Config flow za ARSO Potresi integracijo."""
@@ -22,24 +23,22 @@ class ArsoPotresiConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
         data_schema = vol.Schema({
             vol.Optional("scan_interval", default=DEFAULT_SCAN_INTERVAL): int,
-            vol.Optional("history_days", default=7): int,
+            vol.Optional("history_days", default=DEFAULT_HISTORY_DAYS): int,
         })
         return self.async_show_form(
             step_id="user", data_schema=data_schema, errors=errors
         )
 
-class OptionsFlowHandler(config_entries.OptionsFlow):
+class OptionsFlowHandler(OptionsFlowWithConfigEntry):
     """Tok možnosti za ARSO Potresi integracijo."""
-    def __init__(self, config_entry):
-        self.config_entry = config_entry
 
     async def async_step_init(self, user_input=None):
         """Upravljanje možnosti."""
         if user_input is not None:
-            return self.async_create_entry(title="", data=user_input)
+            return self.async_create_entry(data=user_input)
 
-    
-        options = self.config_entry.data
+        options = self.config_entry.options or self.config_entry.data
+
         options_schema = vol.Schema({
             vol.Optional(
                 "scan_interval", 
@@ -47,7 +46,7 @@ class OptionsFlowHandler(config_entries.OptionsFlow):
             ): int,
             vol.Optional(
                 "history_days", 
-                default=options.get("history_days", 7)
+                default=options.get("history_days", DEFAULT_HISTORY_DAYS)
             ): int,
         })
 
@@ -56,6 +55,7 @@ class OptionsFlowHandler(config_entries.OptionsFlow):
             data_schema=options_schema,
             description_placeholders={
                 "scan_interval": options.get("scan_interval", DEFAULT_SCAN_INTERVAL),
-                "history_days": options.get("history_days", 7),
+                "history_days": options.get("history_days", DEFAULT_HISTORY_DAYS),
             },
         )
+
