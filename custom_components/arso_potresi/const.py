9232909e337503f0fd8d43991a25845671fd3ba0
Andrej <44124120+andrejs2@users.noreply.github.com>
2025-08-07 14:52:30 +0200

    Update const.py

--- custom_components/arso_potresi/const.py
+++ custom_components/arso_potresi/const.py
@@ -1,3 +1,4 @@
 DOMAIN = "arso_potresi"
 DEFAULT_API_URL = "https://potresi.arso.gov.si/sc/potresi/public"
 DEFAULT_SCAN_INTERVAL = 5  # privzeto osveževanje vsakih 5 minut
+DEFAULT_HISTORY_DAYS = 7
